82b18206553638e41f73d8f898dbcaca39026ebe
Elliothc13 <19264259@studentmail.ul.ie>
2022-09-25 22:55:08 +0100

    feat(plotting): :sparkles: added the ability to plot the elevation

---
+++ plot.py
@@ -0,0 +1,16 @@
+import shapefile as shp  # Requires the pyshp package
+import matplotlib.pyplot as plt
+
+sf = shp.Reader("C:/Users/Elliot Cleary/Downloads/YES/Ireland_50.shp")
+
+plt.figure()
+for shape in sf.shapeRecords():
+    x = [i[0] for i in shape.shape.points[:]]
+    y = [i[1] for i in shape.shape.points[:]]
+
+    plt.plot(x,y)
+# ax = plt.axes()
+# ax.set(facecolor = "orange")
+fig = plt.gcf()
+fig.set_size_inches(8, 10)
+plt.show()
