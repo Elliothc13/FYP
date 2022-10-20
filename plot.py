9b305dcc267cdf423f22d22a5106b356202d0fc4
Elliothc13 <19264259@studentmail.ul.ie>
2022-10-20 16:34:14 +0100

    feat(heroku setup): shapefile change

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import shapefile as shp  # Requires the pyshp package
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
-sf = shp.Reader("C:/Users/Elliot Cleary/Downloads/YES/Ireland_50.shp")
+sf = shp.Reader("Ireland_50.shp")
 
 plt.figure()
 for shape in sf.shapeRecords():
