47c4de8112ae6d3d60340231079effc5310d7234
Elliothc13 <19264259@studentmail.ul.ie>
2022-10-20 17:16:34 +0100

    feat(heroku setup): testing heroku

--- plot.py
+++ plot.py
@@ -1,18 +1,20 @@
-import shapefile as shp  # Requires the pyshp package
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
+# import shapefile as shp  # Requires the pyshp package
+# import matplotlib.pyplot as plt
+# import plotly.graph_objects as go
 
-sf = shp.Reader("shapefiles/Ireland_50.shp")
+# sf = shp.Reader("shapefiles/Ireland_50.shp")
 
-plt.figure()
-for shape in sf.shapeRecords():
-    x = [i[0] for i in shape.shape.points[:]]
-    y = [i[1] for i in shape.shape.points[:]]
+# plt.figure()
+# for shape in sf.shapeRecords():
+#     x = [i[0] for i in shape.shape.points[:]]
+#     y = [i[1] for i in shape.shape.points[:]]
 
-    plt.plot(x,y)
-# ax = plt.axes()
-# ax.set(facecolor = "orange")
-fig = plt.gcf()
-fig.set_size_inches(8, 10)
+#     plt.plot(x,y)
+# # ax = plt.axes()
+# # ax.set(facecolor = "orange")
+# fig = plt.gcf()
+# fig.set_size_inches(8, 10)
 
-plt.show()
+# plt.show()
+
+print("hi")
