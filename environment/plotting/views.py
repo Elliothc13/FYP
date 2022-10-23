412a1367ffe22b4febe23e98306c48957c8c2f06
Elliothc13 <19264259@studentmail.ul.ie>
2022-10-23 19:58:00 +0100

    feat(heroku setup): django works for now

--- environment/plotting/views.py
+++ environment/plotting/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello World')
+    return HttpResponse(
+        'hello' + 'world'
+    )
