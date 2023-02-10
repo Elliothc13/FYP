70d63639a506725a664cea3115acbe4ae6211816
Elliothc13 <19264259@studentmail.ul.ie>
2023-02-10 00:46:12 +0000

    feat: :sparkles: working on implimenting the overlay

--- environment/markers/static/JavaScrip/map.js
+++ environment/markers/static/JavaScrip/map.js
@@ -4,8 +4,12 @@ const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const osm = L.tileLayer(url, { attribution: copy });
 const map = L.map("map", { layers: [osm] });
 
-var imageUrl = 'https://github.com/Elliothc13/FYP/blob/main/environment/markers/static/images/0001.png',
-    // imageBounds = [[90, 180], [-90, 180]];
-    imageBounds = [[40.712216, -74.22655], [40.773941, -74.12544]];
-L.imageOverlay(imageUrl, imageBounds).addTo(map);
+var imageUrl = 'https://github.com/Elliothc13/FYP/blob/365eda3979cab87b58338d44786f7fddd4e38410/environment/markers/static/images/0001.png',
+imageBounds = [[52.23912116814022, -7.4398545653698624], [46.05021959320936, 5.781529814782193]];
+// imageBounds = [[40.712216, -74.22655], [40.773941, -74.12544]];
+var imageOverlay = L.imageOverlay(imageUrl, imageBounds, {
+    opacity: 1,
+    zIndex: -1,
+    interactive: true
+}).bindPopup("This is a yellow box").addTo(map);
 map.fitWorld().zoomIn();
--- environment/markers/urls.py
+++ environment/markers/urls.py
@@ -4,8 +4,15 @@ from django.urls import path
 
 from .views import MarkersMapView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = "markers"
 
 urlpatterns = [
     path("map/", MarkersMapView.as_view()),
 ]
+
+if settings.DEBUG:
+ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
